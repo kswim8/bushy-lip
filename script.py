d8da36de15c1207c30cfaf82f644d5cc1ae4544c
Keren Huang <kerenhuang@kerens-mbp-2.wifi.local.cmu.edu>
2024-02-14 11:52:24 -0500

    updated script to merge in leaf size calculations and decouple metrics

--- script.py
+++ script.py
@@ -15,13 +15,36 @@ CHILDREN = "children"
 NAME = "name"
 
 QUERY_NAME = "query_name"
-NODE_NAME = "node_name"
+NODE_INFO = "node_info"
 DEPTH = "depth"
 IS_BUSHY = "is_bushy"
-LEAF_TO_PARENT_RATIO = "(x+y)/z"
-LEAF_RATIO = "x/y"
-LOCAL_LEAF_TO_PARENT_RATIO = "(x'+y')/z"
-LOCAL_LEAF_RATIO = "x'/y'"
+
+LEFT_LEAF_SIZE = "x"
+RIGHT_LEAF_SIZE = "y"
+LEFT_LOCAL_SIZE = "x'"
+RIGHT_LOCAL_SIZE = "y'"
+CURR_SIZE = "z"
+
+LEAF_SIZE = "leaf_size"
+
+
+'''
+Checks if a plan node is JOIN type.
+Explicitly ignores SEMI JOIN.
+'''
+def is_join_node(curr_node):
+    return len(curr_node[CHILDREN]) == 2 and "JOIN" in curr_node[NAME]
+
+
+'''
+Checks if a plan node is a BUSHY JOIN.
+'''
+def is_bushy_join_node(curr_node):
+    children = curr_node[CHILDREN]
+    return is_join_node(curr_node) and \
+           is_join_node(children[0]) and \
+           is_join_node(children[1])
+
 
 '''
 Takes in a .sql file + database connection.
@@ -33,6 +56,7 @@ def get_json_from_file(file, con):
         query += " " + line.strip()
     query_plan = con.sql(query).fetchall()[0][1]
     return json.loads(query_plan)
+
 
 '''
 Takes in a curr_plan_node as root.
@@ -50,8 +74,10 @@ def get_first_join_node(curr_plan_node, depth):
         depth += 1
     return curr_plan_node, depth, True
 
+
 '''
-Assume curr_plan_node is a JOIN node. 
+Assume curr_plan_node is a JOIN node.
+Chases left and right branches to get immediate/local children sizes.
 '''
 def get_local_children_size(curr_plan_node, depth, data, file_name):
     assert("JOIN" in curr_plan_node[NAME])
@@ -67,18 +93,59 @@ def get_local_children_size(curr_plan_node, depth, data, file_name):
     curr_size = curr_plan_node[CARDINALITY]
 
     data[QUERY_NAME].append(file_name)
-    data[NODE_NAME].append(curr_plan_node[NAME])
+    data[NODE_INFO].append(curr_plan_node[NAME])
     data[DEPTH].append(depth)
-    data[IS_BUSHY].append(False)
-    data[LEAF_TO_PARENT_RATIO].append(0)
-    data[LEAF_RATIO].append(0)
-    data[LOCAL_LEAF_TO_PARENT_RATIO].append(pd.NA if not curr_size else (L_size + R_size) / curr_size) 
-    data[LOCAL_LEAF_RATIO].append(pd.NA if not min(L_size, R_size) else max(L_size, R_size) / min(L_size, R_size))
+    data[IS_BUSHY].append(is_bushy_join_node(curr_plan_node))
+    data[LEFT_LEAF_SIZE].append(children[0][LEAF_SIZE])
+    data[RIGHT_LEAF_SIZE].append(children[1][LEAF_SIZE])
+    data[LEFT_LOCAL_SIZE].append(L_size)
+    data[RIGHT_LOCAL_SIZE].append(R_size)
+    data[CURR_SIZE].append(curr_size)
 
     if L_is_join:
         get_local_children_size(L_child, L_depth, data, file_name)
     if R_is_join:
         get_local_children_size(R_child, R_depth, data, file_name)
+
+
+'''
+Modify the JSON object for current plan node to add depth calculation.
+This depth calculation is specific for JOINs in the plan.
+'''
+def set_join_node_depths(curr_plan_node):
+    children = curr_plan_node[CHILDREN]
+    if not children:
+        curr_plan_node[DEPTH] = 0
+        return 0
+    
+    biggest_depth = set_join_node_depths(children[0])
+    if len(children) == 2:
+        biggest_depth = max(biggest_depth, set_join_node_depths(children[1]))
+
+    if is_join_node(curr_plan_node):
+        biggest_depth += 1
+        curr_plan_node[DEPTH] = biggest_depth
+
+    return biggest_depth
+
+
+'''
+Modify the JSON object for current plan node to add leaf children size.
+This leaf children size is the sum of output cardinalities of leaves in subtree.
+'''
+def set_leaf_children_size(curr_plan_node):
+    children = curr_plan_node[CHILDREN]
+    if not children:
+        leaf_size = curr_plan_node[CARDINALITY]
+        curr_plan_node[LEAF_SIZE] = leaf_size
+        return leaf_size
+
+    leaf_children_size = 0
+    for child in children:
+        leaf_children_size += set_leaf_children_size(child)
+
+    curr_plan_node[LEAF_SIZE] = leaf_children_size
+    return leaf_children_size
 
 
 '''
@@ -89,6 +156,8 @@ Based on the query plan, we gather desired data to be placed in data.
 def get_data(file, data, con):
     file_name = file.name
     query_plan = get_json_from_file(file, con)
+    set_join_node_depths(query_plan)
+    set_leaf_children_size(query_plan)
     top_join_node, top_join_depth, is_join = get_first_join_node(query_plan, 0)
     if is_join:
         get_local_children_size(top_join_node, top_join_depth, data, file_name)
@@ -102,13 +171,14 @@ def main():
     # This dictionary will be used to create a pandas DataFrame later
     data = {
         QUERY_NAME: [],
-        NODE_NAME: [],
+        NODE_INFO: [],
         DEPTH: [],
         IS_BUSHY: [],
-        LEAF_TO_PARENT_RATIO: [],
-        LEAF_RATIO: [],
-        LOCAL_LEAF_TO_PARENT_RATIO: [],
-        LOCAL_LEAF_RATIO: [],
+        LEFT_LEAF_SIZE: [],
+        RIGHT_LEAF_SIZE: [],
+        LEFT_LOCAL_SIZE: [],
+        RIGHT_LOCAL_SIZE: [],
+        CURR_SIZE: [],
     }
 
     # Iterate over all files in DIRECTORY
